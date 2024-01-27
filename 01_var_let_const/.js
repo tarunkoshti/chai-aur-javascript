e05f17a3bea90021b9c24b13e85a70d3899c740a
Tarun Koshti <tarunkoshti910@gmail.com>
2024-01-27 13:32:22 +0530

    first commit

---
+++ 01_var_let_const/.js
@@ -0,0 +1,16 @@
+const age = 20
+let name = "tarun"
+var sername = "koshti"
+qualification = "b.tech"
+let a;
+
+// age = 21 // not allowed
+name = "koshti"
+sername = "tarun"
+qualification = "m.tech"
+
+// prefer not to use var 
+// because of issue in block scope and functional scope
+
+// console.log(age, name, sername, qualification);
+console.table([age, name, sername, qualification, a])
