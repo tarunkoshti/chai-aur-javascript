3c238f964dcb06f22c515916d8860a3202b1940a
Tarun Koshti <tarunkoshti910@gmail.com>
2024-01-28 14:01:56 +0530

    types

---
+++ 02_datatypes/.js
@@ -0,0 +1,15 @@
+"use strict" // treat all js code as newer version
+
+alert(3 + 3) // we are using node, not browser
+
+
+// number => 2
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
