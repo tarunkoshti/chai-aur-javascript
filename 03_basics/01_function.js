a6ce75a5b7a6651434487e78c91d29b8c36767e0
Tarun Koshti <tarunkoshti910@gmail.com>
2024-03-03 18:33:04 +0530

    muitiple parameter and object and array in function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -48,3 +48,68 @@ console.log(loginUser()) // undefined
 
 
 
+// higher order function
+
+const firstOrderFunc = () => {
+    console.log("Hello, I am a First order function")
+    return 10;
+}
+const higherOrder = (ReturnFirstOrderFunc) => ReturnFirstOrderFunc();
+console.log(higherOrder(firstOrderFunc))
+
+// first class function
+
+// const handler = () => console.log("This is a click handler function");
+// document.addEventListener("click", handler);                run in browser console
+
+// constructor function
+function abcd() {
+    this.name = "tarun"
+    age = 21
+}
+
+const n = new abcd();
+console.log(n.name)
+console.log(n.age) // undefined
+console.log(this)
+// arrow function
+const arrowFunc1 = (a, b) => a + b; // Multiple parameters
+const arrowFunc2 = a => a * 10; // Single parameter
+const arrowFunc3 = () => { } // no parameters
+
+// detect function is called as constructor
+function Myfunc() {
+    if (new.target) {
+        console.log(new.target)
+        console.log("called with new");
+    } else {
+        console.log("not called with new");
+    }
+}
+
+new Myfunc()
+Myfunc()
+
+// multiple parameter
+function calCartPrice(val1, val2, ...num) {
+    return num
+}
+
+console.log(calCartPrice(200, 300, 400, 500, 600))
+
+// handle object and array in function
+
+const obj = {
+    name: "tarun",
+    age: 21
+}
+handleObject(obj)
+function handleObject(obj) {
+    console.log(`username is ${obj.name} age is ${obj.age}`)
+}
+
+const arr = [1, 345, "yssgab"]
+console.log(handleArray(arr))
+function handleArray(arr) {
+    return arr[1]
+}
