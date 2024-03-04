14a7ebd38dec28bf4797522824c8245ff345c3a9
Tarun Koshti <tarunkoshti910@gmail.com>
2024-03-04 18:17:34 +0530

    local and global scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+// if(true){
+//     var obj = {name : "tarun"}
+// }
+
+// console.log(obj.name)
+
+// function abcd() {
+//    age = 20
+// }
+// console.log(age)
+
+function exampleFunction() {
+    // Local scope
+    let localVar = "I am local";
+    console.log(localVar);
+}
+
+exampleFunction();
+// console.log(localVar)
