519ccff9b40ec7c9f53f1d5007f3c4afd47592f2
Tarun Koshti <tarunkoshti910@gmail.com>
2024-04-03 23:30:38 +0530

    digital clock

---
+++ 07_projects/03_digital_clock/script.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector(".clock")
+
+// let date = new Date();
+// console.log(date.toLocaleTimeString())
+
+setInterval(function () {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000)
